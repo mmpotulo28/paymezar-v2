8a656f69e488b0c242b220de73b0f39897849210
mmpotulo28 <mpotulom28@gmail.com>
2025-08-02 18:23:45 +0200

    feat: update PayNowPage to use session user data in UserProfileCard for dynamic user display

--- app/pay-now/page.tsx
+++ app/pay-now/page.tsx
@@ -3,16 +3,17 @@ import { Tabs, Tab } from "@heroui/tabs";
 import { UserProfileCard } from "@/components/user-profile-card";
 import { RecentTransactions } from "@/components/recent-transactions";
 import { dummyTransactions } from "@/lib/dummy-transactions";
-import { dummyUser } from "@/lib/dummy-user";
 import { Suspense } from "react";
 import RequestPayment from "@/components/payments/request-payment";
 import SendPayment from "@/components/payments/send-payment";
+import { useSession } from "@/context/SessionManager";
 
 export default function PayNowPage() {
+	const { user } = useSession();
 	return (
 		<section className="flex flex-col items-center justify-start min-h-[70vh] py-8 gap-8 w-full h-full">
 			<Suspense fallback={<div>Loading user profile...</div>}>
-				<UserProfileCard user={dummyUser} className="max-w-2xl" />
+				<UserProfileCard user={user} className="max-w-2xl" />
 			</Suspense>
 			<Tabs className="w-full max-w-2xl">
 				<Tab key="send" title="Send Payment" className="w-full flex flex-col items-center">
