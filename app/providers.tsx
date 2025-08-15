497eab0ceca753a580fc18b65fa12e270f8c5edd
mmpotulo28 <mpotulom28@gmail.com>
2025-08-15 21:23:37 +0200

    refactor: Simplify New Relic BrowserAgent instantiation and remove redundant code

--- app/providers.tsx
+++ app/providers.tsx
@@ -17,6 +17,7 @@ let BrowserAgent: typeof import("@newrelic/browser-agent/loaders/browser-agent")
 if (typeof window !== "undefined") {
 	import("@newrelic/browser-agent/loaders/browser-agent").then((mod) => {
 		BrowserAgent = mod.BrowserAgent;
+
 		// The agent loader code executes immediately on instantiation.
 		const options: AgentOptions = {
 			info: {
@@ -65,59 +66,6 @@ if (typeof window !== "undefined") {
 		};
 		new BrowserAgent(options);
 	});
-}
-// Populate using values from NerdGraph
-
-// The agent loader code executes immediately on instantiation.
-if (typeof window !== "undefined") {
-	console.log("iam running on a browser");
-	const options: AgentOptions = {
-		info: {
-			applicationID: process.env.NEXT_PUBLIC_NEW_RELIC_APPLICATION_ID || "",
-			beacon: "bam.nr-data.net",
-			errorBeacon: "bam.nr-data.net",
-			licenseKey: process.env.NEXT_PUBLIC_NEW_RELIC_LICENSE_KEY || "",
-			sa: 1,
-		},
-		init: {
-			ajax: {
-				deny_list: ["bam.nr-data.net"],
-			},
-			distributed_tracing: {
-				allowed_origins: [],
-				cors_use_newrelic_header: false,
-				cors_use_tracecontext_headers: false,
-				enabled: true,
-				exclude_newrelic_header: false,
-			},
-			privacy: {
-				cookies_enabled: true,
-			},
-			session_replay: {
-				autoStart: true,
-				block_selector: "",
-				collect_fonts: true,
-				enabled: true,
-				error_sampling_rate: 100,
-				fix_stylesheets: true,
-				inline_images: false,
-				mask_all_inputs: true,
-				mask_input_options: {},
-				mask_text_selector: "*",
-				preload: true,
-				sampling_rate: 10,
-			},
-		},
-		loader_config: {
-			accountID: process.env.NEXT_PUBLIC_NEW_RELIC_ACCOUNT_ID,
-			agentID: process.env.NEXT_PUBLIC_NEW_RELIC_BROWSER_AGENT_ID,
-			applicationID: process.env.NEXT_PUBLIC_NEW_RELIC_APPLICATION_ID,
-			licenseKey: process.env.NEXT_PUBLIC_NEW_RELIC_LICENSE_KEY,
-			trustKey: process.env.NEXT_PUBLIC_NEW_RELIC_TRUST_KEY,
-		},
-	};
-
-	new BrowserAgent(options);
 }
 
 export interface ProvidersProps {
